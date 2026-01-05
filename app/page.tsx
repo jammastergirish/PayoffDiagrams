2d92edcabe4d3150ef6565f26bbcbed893695ec4
Girish Gupta <girish@girishgupta.com>
2026-01-05 08:24:58 -0800

    Adds footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { PayoffDashboard } from "@/components/payoff-dashboard";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black text-white p-8 dark">
-      <div className="max-w-7xl mx-auto space-y-8">
+    <main className="min-h-screen bg-black text-white p-8 dark flex flex-col">
+      <div className="max-w-7xl mx-auto w-full flex-1 space-y-8">
         <div className="flex flex-col gap-2 border-b border-white/10 pb-6">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl text-white">
             Payoff <span className="text-orange-500">Visualizer</span>
@@ -15,7 +15,14 @@ export default function Home() {
         </div>
         
         <PayoffDashboard />
+
       </div>
+      <footer className="mt-auto border-t border-white/10 pt-6 pb-4 text-center text-sm text-gray-500">
+        Created by{" "}
+        <a href="https://www.girish.xyz" className="hover:text-orange-500">
+          Girish Gupta
+        </a>
+      </footer>
     </main>
   );
 }
